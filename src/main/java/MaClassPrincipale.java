7d59951324dbf4f684963f0c3ad9036381270c58
janobzh <jeankled@yahoo.fr>
2020-01-28 23:07:39 +0100

    initial project version

---
+++ src/main/java/MaClassPrincipale.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import org.springframework.aop.support.AopUtils;
+
+public class MaClassPrincipale {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+
+    }
+}
